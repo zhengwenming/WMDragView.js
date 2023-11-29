c0eeabaf6d729641ca9d2993dba8efcaf291b083
文明 <740747055@qq.com>
2023-11-29 14:23:59 +0000

    fixed bug

--- wmdragview.js
+++ wmdragview.js
@@ -22,7 +22,7 @@ class wmdragview {
     this.readyMove = false // a) 点击浮球触发readeyMove
     this.isMoving = false // b) touchmove && readyMove, 触发isMoveing
     // 1.2 初始化dom元素
-    const dom = domEle instanceof HTMLElement ?domEle :(domEle.indexOf('#')===0?document.querySelector(domEle):document.getElementById(domEle))
+    const dom = domEle instanceof HTMLElement ?domEle :(domEle.indexOf('.')==0?document.querySelector(domEle):document.getElementById(domEle))
     this.dom = dom
     // 1.3 初始化dom样式, 宽高, 屏幕宽高,可拖动范围（由minX、minY、maxX、maxY共同控制）
     dom.style.position = 'fixed'
